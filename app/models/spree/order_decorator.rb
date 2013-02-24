3f2dcd6b225b828bf78d738ca8ac80333e09750a
msiley <mike.siley@gmail.com>
2013-02-24 14:50:58 -0500

    reformatted code, took out a state transition

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -2,88 +2,53 @@ require_relative 'tax_cloud/tax_cloud_transaction'
 require 'spree/calculator/promotion_tax'
 
 Spree::Order.class_eval do
+  has_one :tax_cloud_transaction
 
-   has_one :tax_cloud_transaction
+  self.state_machine.after_transition :to => :payment,
+	 			      :do => :lookup_tax_cloud,
+	 			      :if => :tax_cloud_eligible?
 
-
-   self.state_machine.after_transition :to => :payment,
-					      :do => :lookup_tax_cloud,
-					      :if => :tax_cloud_eligible?
-
-   self.state_machine.after_transition :to => :payment, 
+##self.state_machine.after_transition :to => :payment, 
+## 			      :do => :create_tax_charge!
   
-					      :do => :create_tax_charge!
-   
-   self.state_machine.after_transition :to => :complete,
-					     :do => :capture_tax_cloud,
-					     :if => :tax_cloud_eligible?
+  self.state_machine.after_transition :to => :complete,
+	 			     :do => :capture_tax_cloud,
+	 			     :if => :tax_cloud_eligible?
 
+  def tax_cloud_eligible?
+    ship_address.try(:state_id?)
+  end
 
-   def tax_cloud_eligible?
-
-       ship_address.try(:state_id?)
-
-   end
-
-
-   def lookup_tax_cloud
-
-      unless tax_cloud_transaction.nil?
-
-	tax_cloud_transaction.lookup
-
-	Spree::Adjustment.where("originator_id = ?", tax_cloud_transaction.id)
-
-      else
-
-	 create_tax_cloud_transaction
-
-	 tax_cloud_transaction.lookup
-
-	 adjustments.create do |adjustment|
-
-	    adjustment.source = self
-
-	    adjustment.originator = tax_cloud_transaction
-
-	    adjustment.label = 'Tax'
-
-	    adjustment.mandatory = true
-
-	    adjustment.eligible = true
-
-	    adjustment.amount = tax_cloud_transaction.amount 
-
-	 end
-
+  def lookup_tax_cloud
+    unless tax_cloud_transaction.nil?
+	    tax_cloud_transaction.lookup
+      Spree::Adjustment.where("originator_id = ?", tax_cloud_transaction.id)
+    else
+      create_tax_cloud_transaction
+      tax_cloud_transaction.lookup
+      adjustments.create do |adjustment|
+         adjustment.source = self
+         adjustment.originator = tax_cloud_transaction
+         adjustment.label = 'Tax'
+         adjustment.mandatory = true
+         adjustment.eligible = true
+         adjustment.amount = tax_cloud_transaction.amount 
       end
+    end
+  end
 
-   end
+  def capture_tax_cloud
+    return unless tax_cloud_transaction
+    tax_cloud_transaction.capture
+  end
 
- 
-   def capture_tax_cloud
+  def update_with_taxcloudlookup 
+    unless tax_cloud_transaction.nil?
+	    tax_cloud_transaction.lookup 
+    end
+    update_without_taxcloud_lookup 
+  end
 
-      return unless tax_cloud_transaction
-
-      tax_cloud_transaction.capture
-
-   end
-
-   def update_with_taxcloudlookup 
-
-      unless tax_cloud_transaction.nil?
-
-	tax_cloud_transaction.lookup 
-
-      end
-
-      update_without_taxcloud_lookup 
-
-   end
-
-   alias_method :update_without_taxcloud_lookup, :update! 
-   alias_method :update!, :update_with_taxcloudlookup 
-
-
-
+  alias_method :update_without_taxcloud_lookup, :update! 
+  alias_method :update!, :update_with_taxcloudlookup 
 end
--- app/models/spree/tax_cloud/tax_cloud_transaction.rb
+++ app/models/spree/tax_cloud/tax_cloud_transaction.rb
@@ -5,127 +5,73 @@
 require 'spree/tax_cloud'
 require 'spree/tax_cloud/tax_cloud_cart_item'
 require_dependency 'spree/order'
+
 module Spree
-
   class TaxCloudTransaction < ActiveRecord::Base
+    belongs_to :order
+    validates :order, :presence => true
+    has_one :adjustment, :as => :originator
+    has_many :cart_items, :class_name => 'TaxCloudCartItem', :dependent => :destroy
 
-      belongs_to :order
+    # called when order updates adjustments
+    def update_adjustment(adjustment, source)
+	    adjustment.update_attribute_without_callbacks(:amount, amount)
+    end
 
-      validates :order, :presence => true
+    def amount
+	    cart_items.sum(&:amount)
+    end
 
-      has_one :adjustment, :as => :originator
+    def lookup
+	    begin
+        create_cart_items
+        response = tax_cloud.lookup(self)
+        raise 'Tax Cloud Lookup Error' unless response.success?
+        transaction do
+	        unless response.body[:lookup_response][:lookup_result][:messages].nil?
+	          self.message = response.body[:lookup_response][:lookup_result][:messages][:response_message][:message]
+	        end
+	        self.save
+	        response_cart_items = Array.wrap response.body[:lookup_response][:lookup_result][:cart_items_response][:cart_item_response]
+	        response_cart_items.each do |response_cart_item|
+	          cart_item = cart_items.find_by_index(response_cart_item[:cart_item_index].to_i)
+	          cart_item.update_attribute(:amount, response_cart_item[:tax_amount].to_f)
+	        end
+	      end
+	    end
+    end
 
-      has_many :cart_items, :class_name => 'TaxCloudCartItem', :dependent => :destroy
+    def capture
+	    tax_cloud.capture(self)
+    end
 
-      # called when order updates adjustments
+  private
 
-      def update_adjustment(adjustment, source)
+    def tax_cloud
+	    @tax_cloud ||= TaxCloud.new
+    end
 
-	 adjustment.update_attribute_without_callbacks(:amount, amount)
-
-      end
-
-      def amount
-
-	 cart_items.sum(&:amount)
-
-      end
-
-      def lookup
-
-	 begin
-
-	    create_cart_items
-
-	    response = tax_cloud.lookup(self)
-
-	    raise 'Tax Cloud Lookup Error' unless response.success?
-
-
-	    transaction do
-
-	       unless response.body[:lookup_response][:lookup_result][:messages].nil?
-
-		  self.message = response.body[:lookup_response][:lookup_result][:messages][:response_message][:message]
-
-	       end
-
-	       self.save
-
-	       response_cart_items = Array.wrap response.body[:lookup_response][:lookup_result][:cart_items_response][:cart_item_response]
-
-	       response_cart_items.each do |response_cart_item|
-
-		  cart_item = cart_items.find_by_index(response_cart_item[:cart_item_index].to_i)
-
-		  cart_item.update_attribute(:amount, response_cart_item[:tax_amount].to_f)
-
-	       end
-
+    def create_cart_items
+	    cart_items.clear
+	    index = 0
+	    order.line_items.each do |line_item|
+	      cart_items.create!({
+          :index     => (index += 1),
+          :tic       => Spree::Config.taxcloud_product_tic, 
+          :sku       => line_item.variant.sku.presence || line_item.variant.id,
+          :quantity  => line_item.quantity,
+          :price     => line_item.price.to_f,
+          :line_item => line_item
+        })
 	    end
 
-
-	 end
-
-      end
-
-      def capture
-
-
-	    tax_cloud.capture(self)
-
-      end
-
-      private
-
-      def tax_cloud
-
-	 @tax_cloud ||= TaxCloud.new
-
-      end
-
-      def create_cart_items
-
-	 cart_items.clear
-
-	 index = 0
-
-	 order.line_items.each do |line_item|
-
-	       cart_items.create!({
-
-	       :index => (index += 1),
-
-	       :tic => Spree::Config.taxcloud_product_tic , 
-
-	       :sku => line_item.variant.sku.presence || line_item.variant.id,
-
-	       :quantity => line_item.quantity,
-
-	       :price => line_item.price.to_f,
-
-	       :line_item => line_item
-
-	    })
-
-	 end
-
-	    cart_items.create!({  
-
-	    :index => (index += 1),
-
-	    :tic =>  Spree::Config.taxcloud_shipping_tic ,  
-
-	    :sku => 'SHIPPING',
-
-	    :quantity => 1,
-
-	    :price => order.ship_total.to_f
-
-	 })
-
-      end
-
-   end
-
+      cart_items.create!({  
+        :index => (index += 1),
+        :tic =>  Spree::Config.taxcloud_shipping_tic ,  
+        :sku => 'SHIPPING',
+        :quantity => 1,
+        :price => order.ship_total.to_f
+      })
+    end
+  end
 end
